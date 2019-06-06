a640fb24bbc09766d58187780e4cfc4ca55de75e
Rahul <rahul13ramesh@gmail.com>
2019-06-06 13:33:28 +0530

    added conditional execution

--- startSlave.sh
+++ startSlave.sh
@@ -1,7 +1,10 @@
 pip install --user flask
 pip install --user portalocker
 cd slave
-nohup python -u api.py >err.log &
+varc=($(ps -u iitm | grep python | wc -l))
+if [ $varc -ge 1 ]; then
+	nohup python -u api.py >err.log &
+fi
 cd ..
 sleep 5
 ps ax | grep api.py
